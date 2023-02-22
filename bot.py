6d6274693fe60aef86e907126b932b25b0826d13
NNKtv28 <nikitapollagrande@gmail.com>
2023-02-22 10:49:24 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,15 @@
 import os
 import sys
+import discord
+import logging
 from dotenv import load_dotenv
 from discord.ext import commands
 from pathlib import Path
-import discord
 
 
 load_dotenv()
 intents = discord.Intents.all()
-
-#colour codes
+#color codes
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -24,22 +24,15 @@ class bcolors:
 
 class Bot(commands.Bot):
     def __init__(self):
-        super().__init__(command_prefix='!', intents=intents, activity=discord.Activity(type=discord.ActivityType.listening, name="Work in progress"))
+        super().__init__(command_prefix=os.getenv("prefix"), intents=intents, activity=discord.Activity(type=discord.ActivityType.listening, name="Use ?help to see my commands"))
 
     #start the  bot
     async def on_ready(self):
         print(bcolors.OKGREEN + f"{self.user.name} has connected to Discord" + bcolors.ENDC + "")
         
         
-    #sends message if user sends a  command that doesnt exist
-#    async def on_command_error(self, ctx, error):
-#        if isinstance(error, commands.CommandNotFound):
-#            await ctx.send(f"The command '{ctx.message.content}' does not exist.")
-#            print(bcolors.WARNING + f"The command '{ctx.message.content}' does not exist." + bcolors.ENDC + "")
-
-    #checks in the cogs folder for modules, All  modules  must go into this folder, otherwise it wont  work 
     async def setup_hook(self) -> None:
-        print("Its working")
+        print(bcolors.OKGREEN + "Loading cogs" + bcolors.ENDC + "")
         for filepath in Path("./cogs").glob("**/*.py"):
             cog_name = Path(filepath).stem
             
@@ -49,11 +42,12 @@ class Bot(commands.Bot):
             except Exception as e:
                 print(bcolors.FAIL + f"Failed to load module '{filepath}': {e}")
         print(bcolors.OKGREEN + "Cogs Succesfully loaded" + bcolors.ENDC + "")
-        
 
 #checks  the  bot token, if valid, it will start  the bot, else it  will return an error  through console
 try:
-    Bot().run(os.getenv("token"))
+    #added login method, all errors will be shown in a file called discord.log in the same folder as this file, debug modes are INFO and DEBUG, if you arent familiarized with this keep it as it is
+    handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+    Bot().run(os.getenv("token"), log_handler=handler, log_level=logging.INFO)
 except discord.LoginFailure:
     print(bcolors.FAIL + "Invalid Discord Token. Please check your .env file." + bcolors.ENDC + "")
     sys.exit()
